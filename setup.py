e2ac0d6de8f53005de431981c4e99427d7e47b9b
Kefkius <kefkius@mail.com>
2015-02-18 19:38:01 -0500

    NMC - file names and paths

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup
 
 setup(
-    name="electrum-server",
+    name="electrum-nmc-server",
     version="0.9",
-    scripts=['run_electrum_server','electrum-server'],
+    scripts=['run_electrum_nmc_server','electrum-nmc-server'],
     install_requires=['plyvel','jsonrpclib', 'irc>=11'],
     package_dir={
-        'electrumserver':'src'
+        'electrumnmcserver':'src'
         },
     py_modules=[
-        'electrumserver.__init__',
-        'electrumserver.utils',
-        'electrumserver.storage',
-        'electrumserver.deserialize',
-        'electrumserver.networks',
-        'electrumserver.blockchain_processor',
-        'electrumserver.server_processor',
-        'electrumserver.processor',
-        'electrumserver.version',
-        'electrumserver.ircthread',
-        'electrumserver.stratum_tcp',
-        'electrumserver.stratum_http'
+        'electrumnmcserver.__init__',
+        'electrumnmcserver.utils',
+        'electrumnmcserver.storage',
+        'electrumnmcserver.deserialize',
+        'electrumnmcserver.networks',
+        'electrumnmcserver.blockchain_processor',
+        'electrumnmcserver.server_processor',
+        'electrumnmcserver.processor',
+        'electrumnmcserver.version',
+        'electrumnmcserver.ircthread',
+        'electrumnmcserver.stratum_tcp',
+        'electrumnmcserver.stratum_http'
     ],
-    description="Bitcoin Electrum Server",
+    description="Namecoin Electrum Server",
     author="Thomas Voegtlin",
     author_email="thomasv1@gmx.de",
     license="GNU Affero GPLv3",
     url="https://github.com/spesmilo/electrum-server/",
-    long_description="""Server for the Electrum Lightweight Bitcoin Wallet"""
+    long_description="""Server for the Electrum-NMC Lightweight Namecoin Wallet"""
 )
 
 
